97b4e19d7c8855610cfd46133bdb0dcc7feb1e0f
Eddie Lee <eddieberklee@gmail.com>
2015-12-11 01:40:20 -0800

    Face, title, description for splash screen animated

--- app/src/main/java/com/compscieddy/compscieddy/SplashActivity.java
+++ app/src/main/java/com/compscieddy/compscieddy/SplashActivity.java
@@ -1,13 +1,51 @@
 package com.compscieddy.compscieddy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
+
+  @Bind(R.id.eddie_face) ImageView mEddieFace;
+  @Bind(R.id.eddie_title) TextView mEddieTitle;
+  @Bind(R.id.greeting_message) TextView mGreetingMessage;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
+    ButterKnife.bind(this);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    mEddieFace.setScaleX(0f);
+    mEddieFace.setScaleY(0f);
+
+    mEddieFace.animate()
+        .setInterpolator(new OvershootInterpolator(2.3f))
+        .scaleX(1.0f)
+        .scaleY(1.0f)
+        .setDuration(1000)
+        .setStartDelay(1000);
+
+    mEddieTitle.animate()
+        .translationY(0)
+        .alpha(1)
+        .setInterpolator(new OvershootInterpolator(1.9f))
+        .setDuration(700)
+        .setStartDelay(1500);
+
+    mGreetingMessage.animate()
+        .alpha(1f)
+        .setDuration(2500)
+        .setStartDelay(2600);
   }
 }
---
+++ app/src/main/java/com/compscieddy/compscieddy/Util.java
@@ -0,0 +1,56 @@
+package com.compscieddy.compscieddy;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.widget.Toast;
+
+/**
+ * Created by elee on 11/8/15.
+ */
+public class Util {
+
+  public static void applyColorFilter(Drawable drawable, int color) {
+    applyColorFilter(drawable, color, false);
+  }
+
+  public static void applyColorFilter(Drawable drawable, int color, boolean mutate) {
+    drawable.clearColorFilter();
+    PorterDuff.Mode mode = (color == Color.TRANSPARENT ? PorterDuff.Mode.SRC_ATOP : PorterDuff.Mode.SRC_IN);
+    if (mutate) {
+      drawable.mutate().setColorFilter(color, mode);
+    } else {
+      drawable.setColorFilter(color, mode);
+    }
+  }
+
+  public static void showToast(Context context, String message) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+  }
+
+  /** Time-Related */
+  public static String militaryTimeToAMPM(int militaryTime) {
+    boolean isAM = (militaryTime / 12) < 1;
+    String amPM = isAM ? "am" : "pm";
+    int adjustedTime = militaryTime % 12;
+    if (adjustedTime == 0) {
+      if (militaryTime / 12 == 1) {
+        adjustedTime = 12;
+      } else if (militaryTime / 12 == 1) {
+        adjustedTime = 24;
+      }
+    }
+    return String.valueOf(adjustedTime) + amPM;
+  }
+
+  public static int dpToPx(int dp) {
+    return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+  }
+
+  public static int pxToDp(int px) {
+    return (int) (px / Resources.getSystem().getDisplayMetrics().density);
+  }
+
+}
